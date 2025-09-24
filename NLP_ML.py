a34a6214c44b30123848680870ce443f11739012
Alaric <teo.alaric@gmail.com>
2025-09-24 17:35:34 +0800

    Update NLP_ML.py

--- NLP_ML.py
+++ NLP_ML.py
@@ -32,7 +32,12 @@ accuracy = model.score(xtest_tfidf, Y_test)
 print(accuracy)
 
 def analyse(cleaned_text, emails, domains, urls, ips):
-    # Use case
+    # Ensure none of the input lists are empty
+    cleaned_text = cleaned_text if cleaned_text else [""]
+    emails = emails if emails else [""]
+    domains = domains if domains else [""]
+    urls = urls if urls else [""]
+    ips = ips if ips else [""]
     new_input = [ct + em + dom + url + ip for ct, em, dom, url, ip in zip(cleaned_text, emails, domains, urls, ips)]
     
     # Vectorize the new input
@@ -49,4 +54,5 @@ def analyse(cleaned_text, emails, domains, urls, ips):
     '''
 
     return probability[0][1]
+
 
