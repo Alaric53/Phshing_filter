265b4633df4ecc273696e1832f2fea96a2d8ade0
Alaric <teo.alaric@gmail.com>
2025-10-09 23:06:51 +0800

    Update ml_train.py

--- ml_train.py
+++ ml_train.py
@@ -19,8 +19,39 @@ df.urls = df.urls.fillna('')
 df.ips = df.ips.fillna('')
 inputs = df.domains + df.body + df.emails + df.urls + df.ips
 
+#Testing different configurations
+i=0
+max_i=0
+accuracy = 0
+stored_accuracy = 0
+MAX_TRIES = 3000
+while (accuracy < 1 and i < MAX_TRIES):
+    #Split Training(70%) and Testing(30%) Data
+    X_train, X_test, Y_train, Y_test = train_test_split(inputs,target,test_size=0.3,random_state=i)
+
+    #Training
+    xtrain_tfidf = vectorizer.fit_transform(X_train)
+    model.fit(xtrain_tfidf, Y_train)
+
+    #Testing
+    xtest_tfidf = vectorizer.transform(X_test)
+
+    #Results
+    accuracy = model.score(xtest_tfidf, Y_test)
+    
+    if accuracy > stored_accuracy:
+        stored_accuracy = accuracy
+        print(accuracy)
+        #update max
+        max_i=i
+        print(i)
+    
+    i += 1
+
+
+#Saving best configuration
 #Split Training(70%) and Testing(30%) Data
-X_train, X_test, Y_train, Y_test = train_test_split(inputs,target,test_size=0.3,random_state=2524)
+X_train, X_test, Y_train, Y_test = train_test_split(inputs,target,test_size=0.3,random_state=max_i)
 
 #Training
 xtrain_tfidf = vectorizer.fit_transform(X_train)
