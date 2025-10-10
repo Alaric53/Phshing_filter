cd821604d39bb4b0557860bd97cc0a3e9e7c30dc
Alaric <teo.alaric@gmail.com>
2025-10-10 18:04:05 +0800

    Update txtclean.py

--- txtclean.py
+++ txtclean.py
@@ -3,7 +3,7 @@ from datacleaning import datacleaning
 
 clean = datacleaning()
 #Must be in /Phishing_filter directory when executing
-clean.cleanfile("testingdata.txt")
+clean.cleanfile("trainingingdata.txt")
 #clean.cleanfile("invalid.txt")
 #cleaned_text, emails, domains, urls, ips = clean.cleantext("urgent urgently" )
 #print(cleaned_text)
