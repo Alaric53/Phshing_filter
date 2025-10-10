4aa702d886566b1616514d4ff9a8d20a61a393c7
Alaric <teo.alaric@gmail.com>
2025-10-10 18:31:04 +0800

    Update ml_processor.py

--- ml_processor.py
+++ ml_processor.py
@@ -4,11 +4,11 @@ def analyse(loaded_model_data, cleaned_text="", emails="", domains="", urls="", 
     vectorizer = loaded_model_data['vectorizer']
     
     # Ensure none of the input lists are empty
-    cleaned_text_str = " ".join(cleaned_text) if isinstance(cleaned_text, list) and cleaned_text else cleaned_text
-    emails_str = " ".join(emails) if isinstance(emails, list) and emails else email
-    domains_str = " ".join(domains) if isinstance(domains, list) and domains else domains
-    urls_str = " ".join(urls) if isinstance(urls, list) and urls else str(urls) else urls
-    ips_str = " ".join(ips) if isinstance(ips, list) and ips else str(ips) else ips
+    cleaned_text_str = " ".join(cleaned_text) if isinstance(cleaned_text, list) else str(cleaned_text)
+    emails_str = " ".join(emails) if isinstance(emails, list) else str(email)
+    domains_str = " ".join(domains) if isinstance(domains, list) else str(domains)
+    urls_str = " ".join(urls) if isinstance(urls, list) else str(urls) else str(urls)
+    ips_str = " ".join(ips) if isinstance(ips, list) else str(ips) else str(ips)
     
     # Combine all text features into a single string
     combined_text = " ".join([cleaned_text_str, emails_str, domains_str, urls_str, ips_str])
@@ -28,6 +28,7 @@ def analyse(loaded_model_data, cleaned_text="", emails="", domains="", urls="", 
     
     #Return probability(Positive%)
     return probability[0][1]
+
 
 
 
