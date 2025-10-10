bd7af84a4c40b6ab3312a6145b0ce22948ea0b7b
Alaric <teo.alaric@gmail.com>
2025-10-10 17:32:30 +0800

    Update ml_processor.py

--- ml_processor.py
+++ ml_processor.py
@@ -3,17 +3,17 @@ from sklearn.linear_model import LogisticRegression
 # Using unigrams, bigrams, and trigrams and setting maximum document frequency of 90%
 model = LogisticRegression()
 
-def analyse(loaded_model_data="", cleaned_text="", emails="", domains="", urls="", ips=""):
+def analyse(loaded_model_data, cleaned_text="", emails="", domains="", urls="", ips=""):
     # Extract model and vectorizer from the loaded data
     model = loaded_model_data['model']
     vectorizer = loaded_model_data['vectorizer']
     
     # Ensure none of the input lists are empty
-    cleaned_text_str = " ".join(cleaned_text) if isinstance(cleaned_text, list) and cleaned_text 
-    emails_str = " ".join(emails) if isinstance(emails, list) and emails
-    domains_str = " ".join(domains) if isinstance(domains, list) and domains
-    urls_str = " ".join(urls) if isinstance(urls, list) and urls else str(urls)
-    ips_str = " ".join(ips) if isinstance(ips, list) and ips else str(ips)
+    cleaned_text_str = " ".join(cleaned_text) if isinstance(cleaned_text, list) and cleaned_text else cleaned_text
+    emails_str = " ".join(emails) if isinstance(emails, list) and emails else email
+    domains_str = " ".join(domains) if isinstance(domains, list) and domains else domains
+    urls_str = " ".join(urls) if isinstance(urls, list) and urls else str(urls) else urls
+    ips_str = " ".join(ips) if isinstance(ips, list) and ips else str(ips) else ips
     
     # Combine all text features into a single string
     combined_text = " ".join([cleaned_text_str, emails_str, domains_str, urls_str, ips_str])
@@ -33,4 +33,5 @@ def analyse(loaded_model_data="", cleaned_text="", emails="", domains="", urls="
     
     #Return probability(Positive%)
     return probability[0][1]
+
 
