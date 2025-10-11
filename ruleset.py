ff6fcd0b1fc6a82c22a52e71b12ef71e181a6969
Decoraze <aurelius01234@gmail.com>
2025-10-11 12:43:35 +0800

    changes tro ruleset.py

--- ruleset.py
+++ ruleset.py
@@ -70,16 +70,12 @@ def levenshtein_distance(a,b):      #paypal vs paypa1
 
 def lookalike_domain_check(domain):   
     
-    pattern = r"https?://(?:www.)?(.*)" 
-    domain_only = re.sub(pattern, r"\1", domain)
-    
-    for i in domain_only.split(' '):                  # exact match = safe
+    for i in domain:                  # exact match = safe
         if i not in SAFE_DOMAINS:
             for safe in SAFE_DOMAINS:
                 distance = levenshtein_distance(i, safe)
                 if distance <= 2:   # small difference means it is suspicious
                     return 10
-    
     return 0
 
 
@@ -92,13 +88,13 @@ def suspicious_url_detection(body):
     return score, suspicious_urls
 
 
-def calculator(sender: str, subject: str, body: str, urlIP: str) -> tuple:  #Main ruleset score calculator returns ruleset score, keyword_count
+def calculator(sender: str, subject: str, body: str, urlIP: str, domains: list) -> tuple:  #Main ruleset score calculator returns ruleset score, keyword_count
     sender_domain = sender.split("@")[-1]                                   #calculations done with ruleset
     domain_score = safe_domain_check(sender)
     keyword_count = suspicious_keyword_check(subject, body)
     keyword_score = min(15, keyword_count)
     position_score = min(15, keyword_position_scoring(subject, body))
-    lookalike_score = lookalike_domain_check(urlIP)
+    lookalike_score = lookalike_domain_check(domains)
     url_score, suspicious_urls = suspicious_url_detection(urlIP)
     url_score = min(15, url_score)
 
@@ -116,7 +112,7 @@ def process_email_and_score(cleaned_text, emails, domains, urls, ips):
     # convert list of urls + ips into one string
     urlIP = " ".join((urls or []) + (ips or []))
 
-    return calculator(sender, subject, body, urlIP)
+    return calculator(sender, subject, body, urlIP,domains)
 '''
 def main():
     print(process_email_and_score("Phishing Phishing Phishing Phishing Phishing Phishing Phishing Phishing ", [], [], [], []))
